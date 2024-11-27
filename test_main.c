488c975c27e89a5bbb4db266f6844f638f724470
prutha <prutha2005@gmail.com>
2024-11-27 01:07:44 -0500

    adding accuracyChecker test function

--- test_main.c
+++ test_main.c
@@ -4,4 +4,15 @@
 
 int main() {
 
+    //testing checkAccuracy
+    // run "cat accuracy_correct.txt | ./ctest OR cat accuracy_incorrect.txt" to check
+    char * s = "sample string";
+    int accuracy = checkAccuracy(s);
+    if (accuracy == 1){
+        printf("\ncorrect output for accuracy\n");
+    }
+    else{
+        printf("\nincorrect output for accuracy\n");
+    }
 }
+
--- type.h
+++ type.h
@@ -2,3 +2,10 @@
 bool level (int level);
 void progress_bar(int completed, int total);
 void startPlay();
+
+//included for the test_main
+float checkAccuracy (char * s);
+float sentenceScore (float accuracy, float time);
+float levelScore (float scores[], int size);
+bool isPass(int level, float level_score);
+
