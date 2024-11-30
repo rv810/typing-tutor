c6b36eced6a06b5a90ac2c0a315a237543b6dd4d
Yug-Vashisth <vashisty@mcmaster.ca>
2024-11-30 18:19:07 -0500

    added comments to progBAr, made ASCII art better

--- progBAr.c
+++ progBAr.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <unistd.h> //include statements
+/**
+ * progress_bar - Displays a textual progress bar.
+ *
+ * Parameters:
+ *  int completed: The current progress or number of completed units.
+ *  int total: The total number of units to be completed.
+ *
+ * Description:
+ *  This function displays a progress bar in the terminal, showing the
+ *  proportion of completed units relative to the total units. The progress bar
+ *  is 70 characters wide by default, with `#` which shows how much of the bar has been filled.
+ */
 
 void progress_bar(int completed, int total) {
-    int bar_width = 70;
-    int progress = (completed * bar_width) / total;
+    int bar_width = 70; //sets width of progress bar
+    int progress = (completed * bar_width) / total; //tracks progress
 
     printf("[");
     for (int i = 0; i < bar_width; ++i) {
         if (i < progress) {
-            printf("#");
+            printf("#"); //prints $ to show how much has been completed
         } else {
             printf(" ");
         }
     }
-    printf("] %d%%\r", (completed * 100) / total);
+    printf("] %d%%\r", (completed * 100) / total); //prints out progress bar % of completion
     fflush(stdout);
-    printf("\n"); 
+    printf("\n"); //moves to new line after completing bar
 }
 
 
