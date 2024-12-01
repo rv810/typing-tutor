28f30f7716e958106780859883d2162708a24000
rv810 <vanir1@mcmaster.ca>
2024-12-01 13:05:14 -0500

    Added comments

--- type_utilities.c
+++ type_utilities.c
@@ -15,6 +15,13 @@ bool level (int level);
 char *generateSentence(int level, int sentence);
 float checkAccuracy (char * s);
 
+/* help
+ *
+ * Parameters:
+ * None
+ *
+ * Description: Displays the help message for the program.
+ */
 void help() {
     printf("KEYBOARD WARRIOR (Version v1.0.0)\n\n");
     printf("Usage: type \n\n");
@@ -44,6 +51,17 @@ void help() {
     printf("    Display this help message.\n\n");
 }
 
+/* sentenceScore
+ *
+ * Parameters:
+ * - accuracy: represents accuracy for current sentence
+ * - time: represents the time taken to type current sentence
+ *
+ * Description: Calculates the users score for the current sentence
+                by taking a weightage of the accuracy and time (80% 
+                and 20% respectively). Uses 1/time so that score
+                decreases as time increases.
+ */
 float sentenceScore (float accuracy, float time) {
         float accuracy_weight = 0.8;
         float time_weight = 0.2;
@@ -54,6 +72,16 @@ float sentenceScore (float accuracy, float time) {
     	return (accuracy_weight * accuracy) + (time_weight * (1 / time));
 }
 
+/* levelScore
+ *
+ * Parameters:
+ * - scores: represents total sentence scores for that level
+ * - size: represents the size of the scores array
+ *
+ * Description: Calculates the users score for the current level
+                by taking an average off all of their sentence scores
+                and converts the score to a percentage.
+ */
 float levelScore (float scores[], int size) {
         float sum = 0.0;
         for (int i=0; i<size; i++) {
@@ -174,30 +202,31 @@ float checkAccuracy (char * s){
 	char letter = 'a';
 	char temp = 'a';
 	int index = 0; //index when comparing iputted string to expected string
-    int correct_letters = 0; // total number of CORRECT letters the user typed
-    int total_letters = 0; //length of string the user is supposed to type
-    printf("Please type sentence here: ");
+        int correct_letters = 0; // total number of CORRECT letters the user typed
+        int total_letters = 0; //length of string the user is supposed to type
+        
+        printf("Please type sentence here: ");
 
-		//finding length of string
-    while (s[total_letters] != '\0'){
-       	total_letters +=1;
-    }
+        //finding length of string
+        while (s[total_letters] != '\0'){
+                total_letters +=1;
+        }
 
-    while (index<total_letters){
-		scanf(" %c", &letter);
-		if (letter == '.'){
-			correct_letters +=1;
-			break;
-		}
+        while (index<total_letters){
+                        scanf("%c", &letter);
+                        if (letter == '.'){
+                                correct_letters +=1;
+                                break;
+                        }
 
-		if (letter == s[index]){
-    		correct_letters +=1;
-            index +=1;
-		}
-    }
+                        if (letter == s[index]){
+                        correct_letters +=1;
+                index +=1;
+                        }
+        }
 
-    float accuracy = correct_letters/(float)total_letters;
+        float accuracy = correct_letters/(float)total_letters;
 
-    return accuracy;
+        return accuracy;
 }
 
