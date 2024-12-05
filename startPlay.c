aa8e6206c985d14916ff2fc4a0e5f54fece37993
prutha <prutha2005@gmail.com>
2024-12-05 15:34:10 -0500

    fixing bug where game would ask to continue after there were no more levels

--- startPlay.c
+++ startPlay.c
@@ -132,12 +132,14 @@ void startPlay(int argc, char *argv[]) {
             current_level += 1;
         }
 
-        printf("\nWould you like to continue playing (Y to continue, any other key to exit)? ");
-        scanf(" %c", &continue_play);
+        if (current_level < LEVELS){
+			printf("\nWould you like to continue playing (Y to continue, any other key to exit)? ");
+        	scanf(" %c", &continue_play);
 		
-        if (continue_play != 'Y' && continue_play != 'y') {
-            printf("See you later!\n\n");
-            exit(0);
-        }
+        	if (continue_play != 'Y' && continue_play != 'y') {
+            	printf("See you later!\n\n");
+            	exit(0);
+        	}
+		}
     }
 }
